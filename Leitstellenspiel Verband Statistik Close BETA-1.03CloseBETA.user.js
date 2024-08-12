68c478f6c7214a6294f631c4647287cb8f722ac3
CaLaVeRaXGER <f.leibfried@outlook.de>
2024-08-12 11:11:50 +0200

    Update Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js

--- Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
+++ Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
@@ -8,8 +8,8 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @updateURL    https://github.com/CaLaVeRaXGER/LSS-Verband-Statistik/raw/main/Leitstellenspiel%20Verband%20Statistik%20CLOSE%20BETA-1.00CloseBETA.user.js
-// @downloadURL  https://github.com/CaLaVeRaXGER/LSS-Verband-Statistik/raw/main/Leitstellenspiel%20Verband%20Statistik%20CLOSE%20BETA-1.00CloseBETA.user.js
+// @updateURL    https://github.com/CaLaVeRaXGER/Leitstellenspiel-Verband-Statistik/raw/main/Leitstellenspiel%20Verband%20Statistik%20Close%20BETA-1.03CloseBETA.user.js
+// @downloadURL  https://github.com/CaLaVeRaXGER/Leitstellenspiel-Verband-Statistik/raw/main/Leitstellenspiel%20Verband%20Statistik%20Close%20BETA-1.03CloseBETA.user.js
 // ==/UserScript==
 
 (function () {
