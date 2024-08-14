aab81318bae95a23defb913168a78d5ae2c736b0
CaLaVeRaXGER <f.leibfried@outlook.de>
2024-08-14 13:22:25 +0200

    Update Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js

--- Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
+++ Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leitstellenspiel Verband Statistik Close BETA
 // @namespace    http://tampermonkey.net/
-// @version      2.2.5 Close BETA
+// @version      2.2.6 Close BETA
 // @description  Zeigt Statistiken des Verbandes im Leitstellenspiel als ausklappbares Menü an, mit hervorgehobenen Zahlen und strukturierter, einklappbarer Skript-Info, ohne das Menü zu schließen.
 // @author       Fabian (Capt.BobbyNash)
 // @match        https://www.leitstellenspiel.de/
@@ -15,7 +15,7 @@
 (function () {
     "use strict";
 
-    const currentVersion = "2.2.5"; // Aktuelle Version des Skripts
+    const currentVersion = "2.2.6"; // Aktuelle Version des Skripts
 
     // Stil für das neue Design hinzufügen
     GM_addStyle(`
@@ -334,7 +334,7 @@
                 `<li><a href="#" style="color: white; font-size: 10px;">Supporter: m75e, twoyears</a></li>`
             );
             scriptInfoContainer.append(
-                `<li><a href="#" style="color: white; font-size: 10px;">Version: 2.2.5 (Close BETA)</a></li>`
+                `<li><a href="#" style="color: white; font-size: 10px;">Version: 2.2.6 (Close BETA)</a></li>`
             );
             scriptInfoContainer.append(
                 `<li><a href="#" style="color: white; font-size: 10px;">Funktionen des Skripts:</a></li>`
