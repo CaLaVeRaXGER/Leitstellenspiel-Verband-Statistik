c89d6c6bb911c693b293b43f7d7dcffd36ed2610
CaLaVeRaXGER <f.leibfried@outlook.de>
2024-08-14 12:57:46 +0200

    Update Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js

--- Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
+++ Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leitstellenspiel Verband Statistik Close BETA
 // @namespace    http://tampermonkey.net/
-// @version      2.2.2 Close BETA
+// @version      2.2.3 Close BETA
 // @description  Zeigt Statistiken des Verbandes im Leitstellenspiel als ausklappbares Menü an, mit hervorgehobenen Zahlen und strukturierter, einklappbarer Skript-Info, ohne das Menü zu schließen.
 // @author       Fabian (Capt.BobbyNash)
 // @match        https://www.leitstellenspiel.de/
@@ -15,7 +15,7 @@
 (function () {
     "use strict";
 
-    const currentVersion = "2.2.2"; // Aktuelle Version des Skripts
+    const currentVersion = "2.2.3"; // Aktuelle Version des Skripts
 
     // Stil für das neue Design hinzufügen
     GM_addStyle(`
@@ -139,7 +139,7 @@
     function notifyUpdate(newVersion) {
         const notificationHtml = `
             <div id="update-notification">
-                Das Skript wurde auf Version ${newVersion} aktualisiert. Die Seite wird neu geladen...
+                Das Skript wurde auf Version ${newVersion} aktualisiert. Bitte laden Sie die Seite neu.
             </div>
         `;
         $("body").append(notificationHtml);
@@ -157,9 +157,6 @@
                     const remoteVersion = remoteVersionMatch[1];
                     if (remoteVersion !== currentVersion) {
                         notifyUpdate(remoteVersion);
-                        setTimeout(() => {
-                            window.location.reload(); // Seite automatisch neu laden nach dem Update
-                        }, 3000);
                     }
                 }
             },
@@ -337,7 +334,7 @@
                 `<li><a href="#" style="color: white; font-size: 10px;">Supporter: m75e, twoyears</a></li>`
             );
             scriptInfoContainer.append(
-                `<li><a href="#" style="color: white; font-size: 10px;">Version: 2.2.2 (Close BETA)</a></li>`
+                `<li><a href="#" style="color: white; font-size: 10px;">Version: 2.2.3 (Close BETA)</a></li>`
             );
             scriptInfoContainer.append(
                 `<li><a href="#" style="color: white; font-size: 10px;">Funktionen des Skripts:</a></li>`
