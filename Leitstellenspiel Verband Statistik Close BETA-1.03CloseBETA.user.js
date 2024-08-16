ea3290448a5a95aa51d36ff441938ab84084a8ff
CaLaVeRaXGER <f.leibfried@outlook.de>
2024-08-17 01:44:23 +0200

    Update Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js

--- Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
+++ Leitstellenspiel Verband Statistik Close BETA-1.03CloseBETA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Leitstellenspiel Verband Statistik Close BETA
 // @namespace    http://tampermonkey.net/
-// @version      3.2 (Hotfix)
+// @version      3.1.2 (Hotfix)
 // @description  Zeigt Statistiken des Verbandes im Leitstellenspiel als ausklappbares Menü an, inklusive eines Spielzeit-Timers und der Berechnung des Gesamttagesverdiensts, der täglich um 0:00 Uhr zurückgesetzt wird. Zeigt auch das Verbandsteam mit Verlinkungen zu den Profilen an.
 // @author       Fabian (Capt.BobbyNash)
 // @match        https://www.leitstellenspiel.de/
@@ -16,7 +16,7 @@
 (function () {
     "use strict";
 
-    const currentVersion = "3.2 (Hotfix)"; // Aktuelle Version des Skripts
+    const currentVersion = "3.1.2 (Hotfix)"; // Aktuelle Version des Skripts
     const updateUrl = "https://github.com/CaLaVeRaXGER/Leitstellenspiel-Verband-Statistik/raw/main/Leitstellenspiel%20Verband%20Statistik%20Close%20BETA-1.03CloseBETA.user.js";
     const updateKey = `lss_update_notice_shown_${currentVersion}`; // Key für localStorage, um anzuzeigen, dass das Popup für diese Version bereits gezeigt wurde
 
@@ -394,6 +394,11 @@
 
             // Speichern, dass das Popup angezeigt wurde
             localStorage.setItem(updateKey, "true");
+
+            // Neu laden der Seite nach 2 Sekunden
+            setTimeout(() => {
+                location.reload();
+            }, 2000);
         });
     }
 
@@ -559,7 +564,7 @@
             });
 
             patchNotesContainer.append(`
-                <h3>Patch-Notes 3.2 (Hotfix)</h3>
+                <h3>Patch-Notes 3.1.2 (Hotfix)</h3>
                 <ul>
                     <li>- Behoben: Das automatische Zurücksetzen der Spielzeit und des Tagesverdiensts um Mitternacht (Ortszeit) funktioniert jetzt korrekt.</li>
                     <li>- Behoben: Die Buttons zum manuellen Zurücksetzen der Spielzeit und des Tagesverdiensts funktionieren jetzt ordnungsgemäß.</li>
@@ -619,7 +624,7 @@
                 `<li><a href="#" style="color: white; font-size: 10px;">Supporter: m75e, twoyears</a></li>`
             );
             scriptInfoContainer.append(
-                `<li><a href="#" style="color: white; font-size: 10px;">Version: 3.2 (Hotfix)</a></li>`
+                `<li><a href="#" style="color: white; font-size: 10px;">Version: 3.1.2 (Hotfix)</a></li>`
             );
             scriptInfoContainer.append(
                 `<li><a href="#" style="color: white; font-size: 10px;">Dieses Skript wurde in Zusammenarbeit mit dem Team "Wir in Baden-Württemberg" erstellt.</a></li>`
